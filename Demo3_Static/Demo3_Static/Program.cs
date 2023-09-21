30ac0b0e55c2f4e8508aef7bf0d1e71db46ee5aa
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2023-09-21 16:02:58 -0600

    demo3

---
+++ Demo3_Static/Demo3_Static/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DistyBall;
+
+namespace Demo3_Static
+{
+
+    class Test
+    {
+        //random...should always be static because we talked about it in class!!
+        public static Random _rand = null;
+        private static int iStaticValue = 20;
+
+        //static property bound to iStaticValue
+        public static int Value => iStaticValue;
+
+        public static int GetRandomNumber(short upperLimit)
+        {
+            return _rand.Next(upperLimit);
+        }
+
+        public static string Complain()
+        {
+            string[] messages = { "Do I have to?", "I'm too tired", "I don't care..." };
+            return messages[GetRandomNumber((short)messages.Length)]; //use static method here
+        }
+
+        //static constructor
+        static Test()
+        {
+            _rand = new Random();
+        }
+    }
+    internal class Program
+    {
+        //this degenerates to a static because there was only ever one instance open...!
+        //Random r = new Random();
+        static void Main(string[] args)
+        {
+            int iVal = Test.Value;
+            Console.WriteLine(Test.Complain());
+            Console.WriteLine(Test.GetRandomNumber(7));
+            Console.WriteLine(CDistyBall.myNum);
+            Console.Read();
+        }
+    }
+}
