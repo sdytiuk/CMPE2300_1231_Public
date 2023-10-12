eba899770f96749737612a7d24e540fe6ecf1d0a
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2023-10-12 11:14:10 -0600

    Added compareDemo
    Contains comparison methods and use with Predicates

---
+++ compareDemo/compareDemo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            List<Thing> list = new List<Thing>();
+
+            
+
+            for (int i = 0; i < rand.Next(10,101); i++)
+            {
+                list.Add(new Thing(rand.Next(100)));
+            }
+
+            //THIS LINE AFFECTS THE DEFAULT SORT ORDER!!
+            Thing.SortType = Thing.ESortType.Descending;
+            //sort using default comparer
+            //default comparer is CompareTo from the class!!
+            list.Sort();
+            //display the things
+            Console.WriteLine("Default Compare:");
+            foreach (Thing thing in list)
+            {
+                Console.Write(thing.X + ", ");
+            }
+            Console.WriteLine();
+
+            //sort using MyThingCompare
+            list.Sort(Thing.MyThingCompare);
+            //display the things
+            Console.WriteLine("MyThingCompare:");
+            foreach (Thing thing in list)
+            {
+                Console.Write(thing.X + ", ");
+            }
+            Console.WriteLine();
+
+
+            //sort using MyThingCompareDesc
+            list.Sort(Thing.MyThingCompareDesc);
+            //display the things
+            Console.WriteLine("MyThingCompareDesc:");
+
+            //display the things
+            foreach (Thing thing in list)
+            {
+                Console.Write(thing.X + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Even things using defined predicate");
+            //only EVEN things!
+            foreach (Thing thing in list.FindAll(Thing.IsEven))
+            {
+                Console.Write(thing.X + ", ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Odd things using anonymous predicate");
+            //only EVEN things!
+            foreach (Thing thing in list.FindAll((x) =>  x.X%2==1))
+            {
+                Console.Write(thing.X + ", ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ compareDemo/compareDemo/Thing.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareDemo
+{
+    internal class Thing : IComparable
+    {
+        public int X { get; private set; }
+        public Thing (int iVal)
+        {
+            X = iVal;
+            SortType = ESortType.Ascending; //default sort
+        }
+        public enum ESortType { Ascending , Descending }; //possible sort options
+        static public ESortType SortType { get; set; }
+
+        public int CompareTo (object obj)
+        {
+            if (!(obj is Thing thing))
+                throw new ArgumentException($"Thing:CompareTo:{nameof(obj)} - not a valid thing");
+            return MyThingCompare(this, thing); //Kaiden's suggestion... leverage below :)
+        }
+
+        internal static int MyThingCompare (Thing a, Thing b)
+        {
+            switch (SortType)
+            {
+                case ESortType.Ascending:
+                    if (a == null && b == null) return 0; // null == null so they are the same
+                    if (a == null) return -1; //null is LESS than anything
+                    if (b == null) return 1;
+                    return a.X.CompareTo(b.X); //using INT sort
+                case ESortType.Descending:
+                    if (a == null && b == null) return 0; // null == null so they are the same
+                    if (a == null) return 1; //null is LESS than anything
+                    if (b == null) return -1;
+                    return -a.X.CompareTo(b.X); //using INT sort
+                default:
+                    break;
+            }
+            return 0;// unknown sort
+            
+        }
+
+        internal static int MyThingCompareDesc(Thing a, Thing b)
+        {
+            if (a == null && b == null) return 0; // null == null so they are the same
+            if (a == null) return 1; //null is LESS than anything
+            if (b == null) return -1;
+            return -a.X.CompareTo(b.X); //using INT sort
+        }
+
+        public static bool IsEven(Thing arg)
+        {
+            if (arg == null) return false; //null is not even
+            return (arg.X % 2) == 0;
+        }
+    }
+}
