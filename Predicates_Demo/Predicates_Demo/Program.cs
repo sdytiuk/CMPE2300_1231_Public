a724d3ea60e00be71ca9f68a54849351991653c6
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2023-10-25 08:49:01 -0600

    demo complete

---
+++ Predicates_Demo/Predicates_Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicates_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Something> someList = new List<Something>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                someList.Add(new Something(random.Next(1,10)));
+
+            }
+
+            //print the values
+            someList.ForEach(x =>  Console.Write($"{x},") );
+
+            //are all numbers greater than five?
+            Console.WriteLine("Are all numbers greater than 5?: " + someList.TrueForAll(Something.IsBiggerThan5));
+
+            //are some numbers greater than five?
+            Console.WriteLine("Are some numbers greater than 5?: " + !someList.TrueForAll(
+                delegate (Something s) { return s.myNum < 6; }));
+
+            //remove all the ones less than 6
+            someList.RemoveAll(itemFromList => itemFromList.myNum < 6);
+
+            //print the values
+            someList.ForEach(x => Console.Write($"{x},")); 
+
+            //are all numbers greater than five?
+            Console.WriteLine("Are all numbers greater than 5?: " + someList.TrueForAll(Something.IsBiggerThan5));
+
+            //are some numbers greater than five?
+            Console.WriteLine("Are some numbers greater than 5?: " + !someList.TrueForAll(
+                delegate (Something s) { return s.myNum < 6; }));
+
+            //sort the values
+            someList.Sort( (right, left) => left.myNum.CompareTo(right.myNum));
+
+            //print the values
+            someList.ForEach(x => Console.Write($"{x},"));
+
+            Console.Read();
+        }
+    }
+}
---
+++ Predicates_Demo/Predicates_Demo/Something.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicates_Demo
+{
+    internal class Something
+    {
+        private int _myNum;
+
+        public int myNum {get { return _myNum; } }
+
+        public Something(int _myNum)
+        {
+            _myNum = _myNum; //useless because _myNum is the parameter
+
+            this._myNum = _myNum; //use this to differentiate
+        }
+
+        //default constructor
+        public Something():this(5)  //the default CTOR is leveraging the custom constructor and inputting 5 as the value
+        {
+        
+        }
+
+        public static bool IsBiggerThan5(Something s)
+        { 
+            return s._myNum > 5; //this predicate can make use of PRIVATE fields!
+        }
+
+        public override string ToString()
+        {
+            return _myNum.ToString();
+        }
+    }
+}
