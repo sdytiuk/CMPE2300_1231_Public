88c5d749713eb0561df95e98bb6d22757dd6456f
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2023-11-07 09:26:55 -0700

    Demo complete

---
+++ Demo_LL_Dictionary/Demo_LL_Dictionary/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Demo_LL_Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNumberToAdd = new System.Windows.Forms.TextBox();
+            this.txtNumberLookingFor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbDictionaryWords = new System.Windows.Forms.ListBox();
+            this.txtDict1 = new System.Windows.Forms.TextBox();
+            this.txtDict2 = new System.Windows.Forms.TextBox();
+            this.btnAddDict = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDefinition = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AddNumberAfter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumberToAdd
+            // 
+            this.txtNumberToAdd.Location = new System.Drawing.Point(355, 55);
+            this.txtNumberToAdd.Name = "txtNumberToAdd";
+            this.txtNumberToAdd.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberToAdd.TabIndex = 2;
+            // 
+            // txtNumberLookingFor
+            // 
+            this.txtNumberLookingFor.Location = new System.Drawing.Point(355, 110);
+            this.txtNumberLookingFor.Name = "txtNumberLookingFor";
+            this.txtNumberLookingFor.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberLookingFor.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pick a number to add after the number entered below the button:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Now presenting the DICTIONARY:";
+            // 
+            // lbDictionaryWords
+            // 
+            this.lbDictionaryWords.FormattingEnabled = true;
+            this.lbDictionaryWords.Location = new System.Drawing.Point(16, 276);
+            this.lbDictionaryWords.Name = "lbDictionaryWords";
+            this.lbDictionaryWords.Size = new System.Drawing.Size(201, 147);
+            this.lbDictionaryWords.TabIndex = 6;
+            this.lbDictionaryWords.SelectedIndexChanged += new System.EventHandler(this.lbDictionaryWords_SelectedIndexChanged);
+            // 
+            // txtDict1
+            // 
+            this.txtDict1.Location = new System.Drawing.Point(544, 250);
+            this.txtDict1.Name = "txtDict1";
+            this.txtDict1.Size = new System.Drawing.Size(234, 20);
+            this.txtDict1.TabIndex = 7;
+            // 
+            // txtDict2
+            // 
+            this.txtDict2.Location = new System.Drawing.Point(544, 285);
+            this.txtDict2.Multiline = true;
+            this.txtDict2.Name = "txtDict2";
+            this.txtDict2.Size = new System.Drawing.Size(234, 95);
+            this.txtDict2.TabIndex = 8;
+            // 
+            // btnAddDict
+            // 
+            this.btnAddDict.Location = new System.Drawing.Point(544, 386);
+            this.btnAddDict.Name = "btnAddDict";
+            this.btnAddDict.Size = new System.Drawing.Size(234, 23);
+            this.btnAddDict.TabIndex = 9;
+            this.btnAddDict.Text = "Add To Dictionary";
+            this.btnAddDict.UseVisualStyleBackColor = true;
+            this.btnAddDict.Click += new System.EventHandler(this.btnAddDict_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Word";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Definition";
+            // 
+            // lblDefinition
+            // 
+            this.lblDefinition.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblDefinition.Location = new System.Drawing.Point(245, 276);
+            this.lblDefinition.Name = "lblDefinition";
+            this.lblDefinition.Size = new System.Drawing.Size(226, 147);
+            this.lblDefinition.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(544, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Display Dictionary To Console";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblDefinition);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAddDict);
+            this.Controls.Add(this.txtDict2);
+            this.Controls.Add(this.txtDict1);
+            this.Controls.Add(this.lbDictionaryWords);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumberLookingFor);
+            this.Controls.Add(this.txtNumberToAdd);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNumberToAdd;
+        private System.Windows.Forms.TextBox txtNumberLookingFor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbDictionaryWords;
+        private System.Windows.Forms.TextBox txtDict1;
+        private System.Windows.Forms.TextBox txtDict2;
+        private System.Windows.Forms.Button btnAddDict;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDefinition;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Demo_LL_Dictionary/Demo_LL_Dictionary/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_LL_Dictionary
+{
+    public partial class Form1 : Form
+    {
+        LinkedList<int> ints = new LinkedList<int>();
+        Random rand = new Random();
+
+        //DICTIONARY stuff
+        Dictionary<string,string> websters = new Dictionary<string,string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //add new elements to the head of the linked list
+            while (ints.Count < 10)
+                ints.AddFirst(rand.Next(100));
+
+            //can you put LinkedList into a List?
+            List<int> list = new List<int>(ints);
+            UpdateListBox();
+
+            //experiment
+            
+
+            
+        }
+
+        public void UpdateListBox()
+        {
+            ShowLLInConsole();
+            listBox1.DataSource = null;
+            listBox1.DataSource = ints.ToList();
+        }
+
+        public void ShowLLInConsole()
+        {
+            foreach (int i in ints)
+                Console.Write(i.ToString() + ", ");
+            Console.WriteLine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numberToFind;
+            int numberToAdd;
+
+            if (!int.TryParse(txtNumberToAdd.Text, out numberToAdd))
+            {
+                Console.Write("Invalid Number");
+                return;
+            }
+            if (!int.TryParse(txtNumberLookingFor.Text, out numberToFind))
+            {
+                Console.Write("Invalid Number");
+                return;
+            }
+
+            //find the number we're looking for in the linked list
+            //NODE TRAVERSAL
+            LinkedListNode<int> scan = ints.First;
+
+            while (scan != null)
+            {
+                if (scan.Value == numberToFind)
+                {
+                    //add a number to the next spot
+                    ints.AddAfter(scan, numberToAdd);
+                    break;
+                }
+                scan = scan.Next;
+            }
+
+            UpdateListBox();
+        }
+
+        private void btnAddDict_Click(object sender, EventArgs e)
+        {
+            //add the word and definition
+            websters.Add(txtDict1.Text, txtDict2.Text);
+
+            //add to word listbox
+            List<string> words = new List<string>(websters.Keys);
+
+            lbDictionaryWords.DataSource = null;
+            lbDictionaryWords.DataSource = words;
+        }
+
+        private void lbDictionaryWords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //how do we get the selected index?
+            string word = lbDictionaryWords?.SelectedValue?.ToString();
+
+            //populate the label
+            if (word != null)
+                lblDefinition.Text = websters[word];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (KeyValuePair<string, string> pair in websters)
+            {
+                Console.WriteLine(pair.Key + ", " + pair.Value);
+            }
+        }
+    }
+}
